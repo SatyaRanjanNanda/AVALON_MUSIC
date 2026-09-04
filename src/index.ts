070390f5d9c5cb65740447fca3e7acb039ac7e4e
SatyaRanjanNanda <satyananda954@gmail.com>
2026-09-05 03:46:15 +0530

    Update Lavalink to private Wispbyte node

--- src/index.ts
+++ src/index.ts
@@ -20,9 +20,9 @@ export const client = new Client({
 
 // Configure Lavalink nodes
 const Nodes = [{
-    name: 'HeavenCloud Node',
-    url: process.env.LAVALINK_URL || 'free-lava.heavencloud.in:4000',
-    auth: process.env.LAVALINK_AUTH || 'heavencloud.in',
+    name: 'Wispbyte Private Node',
+    url: process.env.LAVALINK_URL || '78.154.103.7:16134',
+    auth: process.env.LAVALINK_AUTH || 'youshallnotpass',
     secure: process.env.LAVALINK_SECURE === 'true'
 }];
 
